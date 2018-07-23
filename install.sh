b4e94ecf7c070374cbcc84ad098bb71e29cbfca3
fidusachates <jo385877@students.mimuw.edu.pl>
2018-07-23 10:26:02 +0200

    Add install script

---
+++ install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare -A ignore=(
+    [.git]=1 [README.md]=1 [requirements.txt]=1
+)
+
+
+function link_files {
+    echo "Directory: $1"
+    for f in $(ls -A $1); do
+        if [[ -n "${ignore[$f]}" ]]; then
+            echo "Ignoring: $f"
+        else
+            if [[ -d $1$f ]]; then
+                mkdir -p ~/$1$f
+                link_files "$1$f/" $2
+            else
+                if [[ $2 = 1 ]]; then
+                    rm -f ~/$1$f
+                fi
+                file=$(realpath $1$f)
+                echo "Linking $file with ~/$1$f"
+                ln -s $file ~/$1$f
+            fi
+            
+        fi
+    done
+}
+
+if [ "$1" = "--force" ] || [ "$1" = "-f" ]; then
+    link_files "" 1
+else
+    link_files "" 0
+fi
+
