4598fd397606e7e748686f23a7f01b9ada8f1940
fidusachates <obuchowski.jakubb@gmail.com>
2025-09-09 13:43:51 +0200

    Add unmuting volume when changing volume from muted state

--- .config/i3/audio_raise_volume.sh
+++ .config/i3/audio_raise_volume.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+is_muted=`pamixer --get-mute`
+if [ "$is_muted" == "true" ]; then
+    pamixer -t
+fi
 pamixer -i 5
 notify-send "Volume: `pamixer --get-volume`%" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume
 
--- .config/i3/audio_toggle_mute.sh
+++ .config/i3/audio_toggle_mute.sh
@@ -8,5 +8,3 @@ else
     notify-send "Volume: muted" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume
 fi
 
-# notify-send "Volume: `pamixer --get-volume`%" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume
-
