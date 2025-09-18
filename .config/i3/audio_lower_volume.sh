3a96557abdc1d74b7a19a85fe40cc3f010e02e7f
fidusachates <obuchowski.jakubb@gmail.com>
2025-09-18 14:22:48 +0200

    Fix dunst font, size, add progress bar for brightness and volume and add shortcuts to close notifications

--- .config/i3/audio_lower_volume.sh
+++ .config/i3/audio_lower_volume.sh
@@ -5,5 +5,5 @@ if [ "$is_muted" == "true" ]; then
     pamixer -t
 fi
 pamixer -d 5
-notify-send "Volume: `pamixer --get-volume`%" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume
+notify-send "Volume [`pamixer --get-volume`%]" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume -h int:value:"`pamixer --get-volume`"
 
--- .config/i3/brightness_up.sh
+++ .config/i3/brightness_up.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 xbacklight -inc 10
-notify-send "Brightness: `xbacklight -get`%" -u low -t 1000 -h string:x-canonical-private-synchronous:brightness
+dunstify "Brightness [`xbacklight -get`%]" -u low -t 1000 -h string:x-canonical-private-synchronous:brightness -h int:value:"`xbacklight -get`"
 
