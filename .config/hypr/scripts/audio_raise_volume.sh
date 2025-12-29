d6038ff446553e9cb30ad6e19c5c002b83854a8e
fidusachates <obuchowski.jakubb@gmail.com>
2025-12-29 16:07:39 +0100

    refactor: move hyprland scripts to bin directory

--- .config/hypr/scripts/audio_raise_volume.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-is_muted=`pamixer --get-mute`
-if [ "$is_muted" == "true" ]; then
-    pamixer -t
-fi
-pamixer -i 5
-notify-send "Volume [`pamixer --get-volume`%]" -u low -t 1000 -h string:x-canonical-private-synchronous:audio_volume -h int:value:"`pamixer --get-volume`"
-
